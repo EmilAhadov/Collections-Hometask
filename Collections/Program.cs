c15939803daa01ed6fa353db0748d7844c2a82cd
EmilAhadov <ca.r213.15@ca.local>
2023-04-12 16:12:10 +0400

    need modify

---
+++ Collections/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+
+namespace Collections
+{
+    internal class Program
+    {
+        //-------------------------------------------------------------------------------------------------------
+        //Task1
+        //-------------------------------------------------------------------------------------------------------
+        //Task2
+        //static void Main(string[] args)
+        //{
+        //    Dictionary<string, int> WordsAndAmount = new Dictionary<string, int>();
+        //    List<string> list = new List<string>();
+        //    string sentence = "a a a b b b";
+        //    string[] words = sentence.Split(' ');
+
+        //    for (int i = 0; i < words.Length; i++)
+        //    {
+        //        if (!list.Contains(words[i]))
+        //        {
+        //            list.Add(words[i]);
+        //        }
+
+        //    }
+        //    int[] counts = new int[list.Count];
+        //    int count = 0;
+        //    for (int i = 0; i < list.Count; i++)
+        //    {
+        //        count = 0;
+        //        for (int j = 0; j < words.Length; j++)
+        //        {
+        //            if (words[j] == list[i])
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        counts[i] = count;
+        //    }
+        //    for (int i = 0; i < counts.Length; i++)
+        //    {
+        //        WordsAndAmount.Add(list[i], counts[i]);
+        //    }
+        //    foreach (KeyValuePair<string, int> pair in WordsAndAmount)
+        //    {
+        //        Console.WriteLine(pair.Key);
+        //        Console.WriteLine(pair.Value);
+        //    }
+        //}
+        //-------------------------------------------------------------------------------------------------------
+        //Task3
+        //static void Main(string[] args)
+        //{
+        //    int cursor = 7;
+        //    Console.WriteLine("Please enter the sentence");
+        //    string sentence = Console.ReadLine();
+        //    Stack<string> stack = new Stack<string>();
+        //    stack.Push(sentence);
+        //    bool flag = true;
+        //    do
+        //    {
+        //        Console.WriteLine("What do you want to do: \n" +
+        //            "1.Edit Current Text\n" +
+        //            "2.Exit");
+        //        string test = Console.ReadLine();
+        //        if(test == "1")
+        //        {
+        //            Console.WriteLine("hello");
+        //            Console.SetCursorPosition(cursor+4, cursor);
+        //            Console.WriteLine("      ");
+        //        }
+        //        else
+        //        {
+        //            flag = false;
+        //        }
+        //    } while (flag);
+
+        //}
+        //-------------------------------------------------------------------------------------------------------
+        //Task4
+        //-------------------------------------------------------------------------------------------------------
+        //Task5
+
+    }
+}
+
+
+
+
